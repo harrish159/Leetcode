70fc9ff23b0d67c72a808e999f55e0e48e0f51f0
harrish159 <155507209+harrish159@users.noreply.github.com>
2025-04-02 12:22:30 +0530

    Time: 7095 ms (5.1%), Space: 14.6 MB (5.1%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -2,8 +2,7 @@ class Solution(object):
     def arrayPairSum(self, nums):
         mini=0
         nums.sort()
-        for i in range(0,len(nums)):
-            if(i%2==0):
+        for i in range(0,len(nums),2):
                 for j in range(i+1,len(nums)):
                     mini=mini+min(nums[i],nums[j])
                     break
