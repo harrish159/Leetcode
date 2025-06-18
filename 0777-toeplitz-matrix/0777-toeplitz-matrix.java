1dde0a6e766314a1e658200e01ebd2e4c9b4e179
harrish159 <155507209+harrish159@users.noreply.github.com>
2025-06-18 22:02:28 +0530

    Time: 0 ms (100%), Space: 44.8 MB (8.28%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.java
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.java
@@ -4,11 +4,11 @@ class Solution {
         int n = matrix.length;
         int m= matrix[0].length;
         
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=1;j<m;j++)
+            for(int j=0;j<m-1;j++)
             {
-                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
                     return false;
             }
         }
